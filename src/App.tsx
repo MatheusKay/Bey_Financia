90df70625a43b23beb54511d063da532e78a5594
Matheus Kayque <matheuskayquebatistasilva712@gmail.com>
2024-09-01 21:09:46 -0300

    chore: Apagando arquivos desnecessarios do projeto, sendo eles de Css e ajustes feitos pela a configuração do eslint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
+import { StyleGlobal } from './Styles/global'
+
+import { store } from './store'
+import RoutesPages from './Routes/routes'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <BrowserRouter>
+        <StyleGlobal />
+        <div>
+          <RoutesPages />
+        </div>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
